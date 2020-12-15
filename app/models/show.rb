1d4f8e52b80d1295af77697bae26071916da6095
Michael Fuget <michaelfuget@gmail.com>
2020-12-15 23:54:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show 
     self.find_by(rating: "#{highest_rating}")
   end
+  
+  def self.lowest_rating
+    Show.minimum('rating')
+  end
 end
